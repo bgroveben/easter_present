1843721c8e0d59b642541076acb7fae09b6c6ffd
bgroveben <bgroveben@gmail.com>
2018-04-01 03:34:36 -0700

    Change a bit of style and content

--- css-easter-loading-screen/js/index.js
+++ css-easter-loading-screen/js/index.js
@@ -1,22 +1,22 @@
 //For adding dots to loading
 window.onload = function(){
     var loading = document.getElementById("loading");
-    
+
     function addRedDot(i){
-        i.innerHTML = "<h1>LOADING<span class='dot-red'>.</span></h1>";
+        i.innerHTML = "<h1>! HAPPY EASTER !<span class='dot-red'>.</span></h1>";
         }
         function addYellowDot(i){
-        i.innerHTML = "<h1>LOADING<span class='dot-red'>.</span><span class='dot-yellow'>.</span></h1>";
+        i.innerHTML = "<h1>! HAPPY EASTER !<span class='dot-red'>.</span><span class='dot-yellow'>.</span></h1>";
         }
         function addGreenDot(i){
-        i.innerHTML = "<h1>LOADING<span class='dot-red'>.</span><span class='dot-yellow'>.</span><span class='dot-green'>.</span></h1>";
+        i.innerHTML = "<h1>! HAPPY EASTER !<span class='dot-red'>.</span><span class='dot-yellow'>.</span><span class='dot-green'>.</span></h1>";
         }
         function removeDots(i) {
-        i.innerHTML = "<h1>LOADING</h1>";
+        i.innerHTML = "<h1>! HAPPY EASTER !</h1>";
         }
-    
+
     function timedDots(i) {
-    
+
         setTimeout(function(){
             addRedDot(i)
         }, 1000);
@@ -29,9 +29,9 @@ window.onload = function(){
         setTimeout(function(){
             removeDots(i)
         }, 4000);
-        
+
         }
-    
+
     setInterval(function(){
         timedDots(loading)
     }, 4000);
